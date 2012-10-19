05ef71d1394ebb1eb5a880c1e71d868094c7fac0
Kræn Hansen <post@creen.dk>
2012-10-19 23:23:16 +0200

    Initial commit.

---
+++ commands.py
@@ -0,0 +1,35 @@
+# Here you can create play commands that are specific to the module, and extend existing commands
+
+MODULE = 'OpenARMS-Authentication-CAS'
+
+# Commands that are specific to your module
+
+COMMANDS = ['OpenARMS-Authentication-CAS:hello']
+
+def execute(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+    if command == "OpenARMS-Authentication-CAS:hello":
+        print "~ Hello"
+
+
+# This will be executed before any command (new, run...)
+def before(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+
+# This will be executed after any command (new, run...)
+def after(**kargs):
+    command = kargs.get("command")
+    app = kargs.get("app")
+    args = kargs.get("args")
+    env = kargs.get("env")
+
+    if command == "new":
+        pass
